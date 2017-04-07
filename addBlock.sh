a56c5831ddd40b3ead4d7a34e7de1a8a9853e4e1
Mario Strohmeier <mario.strohmeier2@gmail.com>
2017-04-07 12:00:51 +0200

    addBlock update

--- addBlock.sh
+++ addBlock.sh
@@ -2,5 +2,5 @@
 if [ "$#" -ne 2 ]; then
     echo "Usage: ./addBlock.sh <HTTP Port> <Data>"
 else
-	curl -H "Content-type:application/json" --data '{"data" : "'$2'"}' http://localhost:"$1"/mineBlock
+	curl -H "Content-type:application/json" --data '{"data" : "'"$2"'"}' http://localhost:"$1"/mineBlock
 fi
