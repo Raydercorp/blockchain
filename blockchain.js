a70201a1a01559110a487c99ba92874d52f5fb83
Mario Strohmeier <mario.strohmeier2@gmail.com>
2017-04-08 19:27:35 +0200

    add peer and ad block working

--- blockchain.js
+++ blockchain.js
@@ -44,26 +44,6 @@ initHttpServer = () => {
     app.use('/node_modules', express.static(__dirname + "/node_modules"));
     app.use('/style', express.static(__dirname + "/style"));
 
-    // Add headers
-	app.use(function (req, res, next) {
-
-	    // Website you wish to allow to connect
-	    res.setHeader('Access-Control-Allow-Origin', '*');
-
-	    // Request methods you wish to allow
-	    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-	    // Request headers you wish to allow
-	    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-	    // Set to true if you need the website to include cookies in the requests sent
-	    // to the API (e.g. in case you use sessions)
-	    res.setHeader('Access-Control-Allow-Credentials', true);
-
-	    // Pass to next layer of middleware
-	    next();
-	});
-
     app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain)));
 
     app.post('/mineBlock', (req, res) => {
